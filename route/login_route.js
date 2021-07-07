7e4ac2a6d7c5e25881e980103923f9fad42f3935
sam33na <saminashrestha98@gmail.com>
2021-07-07 12:52:10 +0545

    check username then statemtnt

--- route/login_route.js
+++ route/login_route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Register = require('../model/register_model');
+const Reg = require('../model/register_model');
 const { check, validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken')
+//const jwt = require('jsonwebtoken')
 
 //register user
 router.post('/user/register',  function (req, res) {
@@ -12,15 +12,15 @@ router.post('/user/register',  function (req, res) {
         //valid
         const name = req.body.name;
         const email = req.body.email;
-        const usrnm = req.body.usrnm;
-        const pw = req.body.pw;
-        const phn = req.body.phn;
+        const username = req.body.username;
+        const password = req.body.password;
+        const phone = req.body.phone;
         const role = req.body.role;
-        bcryptjs.hash(pw, 10, function (err, hide) {
-            const store = new Register({ name: name, email: email, usrnm: usrnm, pw: hide, phn: phn, role: role});
+        bcryptjs.hash(password, 10, function (err, hide) {
+            const store = new Register({ name: name, email: email, username: username, password: hide, phone: phone, role: role});
             console.log(hide);
             store.save().then(function (result) {
-                res.status(200).json({ success: true, message: "User registered." }) 
+                res.status(200).json({ success: true, message: "Registeration sucessfull" }) 
             }).catch(function (error) {
                 res.status(500).json({ err: error })
             })
@@ -31,10 +31,11 @@ router.post('/user/register',  function (req, res) {
         res.status(201).json(errors.array());
     }
 })
-
-//login 
 router.post('/user/login', function (req, res) {
     const user = req.body.username;
     const password = req.body.password;
+    Reg.findOne({ username: user }).then(function (dataSave) {
+     
+    })
 })
 module.exports = router;
