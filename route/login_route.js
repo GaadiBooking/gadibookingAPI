fbde9eec5e36b310bcf54f5044c6afda7752ce14
sam33na <saminashrestha98@gmail.com>
2021-07-07 12:38:19 +0545

    varibles for login created

--- route/login_route.js
+++ route/login_route.js
@@ -12,15 +12,15 @@ router.post('/user/register',  function (req, res) {
         //valid
         const name = req.body.name;
         const email = req.body.email;
-        const username = req.body.username;
-        const password = req.body.password;
-        const phone = req.body.phone;
+        const usrnm = req.body.usrnm;
+        const pw = req.body.pw;
+        const phn = req.body.phn;
         const role = req.body.role;
-        bcryptjs.hash(password, 10, function (err, hide) {
-            const store = new Register({ name: name, email: email, username: username, password: hide, phone: phone, role: role});
+        bcryptjs.hash(pw, 10, function (err, hide) {
+            const store = new Register({ name: name, email: email, usrnm: usrnm, pw: hide, phn: phn, role: role});
             console.log(hide);
             store.save().then(function (result) {
-                res.status(200).json({ success: true, message: "Registeration sucessfull" }) 
+                res.status(200).json({ success: true, message: "User registered." }) 
             }).catch(function (error) {
                 res.status(500).json({ err: error })
             })
@@ -34,6 +34,7 @@ router.post('/user/register',  function (req, res) {
 
 //login 
 router.post('/user/login', function (req, res) {
-  
+    const user = req.body.username;
+    const password = req.body.password;
 })
 module.exports = router;
