3812b0275e0f23d8940c1cfd442186e8c0250c6e
sam33na <saminashrestha98@gmail.com>
2021-07-12 18:44:34 +0545

    validation added

--- route/ticket_route.js
+++ route/ticket_route.js
@@ -4,7 +4,13 @@ const Ticket = require('../model/ticket_model');
 
 //add ticket
 router.post('/add/ticket',  function (req, res) {
-   
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+       
+    }
+    else {
+        res.status(201).json(errors.array());
+    }
 })
 
 module.exports = router;
