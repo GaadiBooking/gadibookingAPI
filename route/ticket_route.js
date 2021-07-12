da06fa94dc1f8bab65661bc5adfd380687cca3ad
sam33na <saminashrestha98@gmail.com>
2021-07-12 18:24:23 +0545

    route added for adding ticket

---
+++ route/ticket_route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Ticket = require('../model/ticket_model');
+
+//add ticket
+router.post('/add/ticket',  function (req, res) {
+   
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const mongoose=require('mongoose');
 const express=require('express');
-const cors = require("cors")
 const bodyParser =require('body-parser');
 
 const db= require('./database/db');
 const login_route=require('./route/login_route');
+const ticket_route=require('./route/ticket_route');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
 app.use(express.json());
 app.use(login_route);
+app.use(ticket_route);
 
 app.listen(90);
