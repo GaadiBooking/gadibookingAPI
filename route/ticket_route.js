6b291c39eaae0385955f49039cf4215e722a1e04
sam33na <saminashrestha98@gmail.com>
2021-07-12 19:08:09 +0545

    route added to serer.js

--- route/ticket_route.js
+++ route/ticket_route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Ticket = require('../model/ticket_model');
+const { check, validationResult } = require('express-validator');
 
 //add ticket
 router.post('/add/ticket',  function (req, res) {
