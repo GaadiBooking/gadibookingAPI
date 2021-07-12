1d76467e9bf16741c990bf4f386d7e27450d4d8c
sam33na <saminashrestha98@gmail.com>
2021-07-12 19:34:20 +0545

    ticket added successfully

--- route/ticket_route.js
+++ route/ticket_route.js
@@ -7,7 +7,20 @@ const { check, validationResult } = require('express-validator');
 router.post('/add/ticket',  function (req, res) {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-       
+        const location = req.body.location;
+        const driver = req.body.driver;
+        const price = req.body.price;
+        const seat = req.body.seat;
+        const date = req.body.date;
+        const time = req.body.time;
+        const phone = req.body.phone;
+            const store = new Ticket({ location: location, driver: driver, price: price, seat: seat, date: date, time: time, phone: phone});
+            console.log(driver);
+            store.save().then(function (result) {
+                res.status(200).json({ success: true, message: "Ticket Scheduled Successfully" }) 
+            }).catch(function (error) {
+                res.status(500).json({ err: error })
+            })
     }
     else {
         res.status(201).json(errors.array());
