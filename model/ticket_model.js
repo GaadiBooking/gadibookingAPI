557d8c7ccdb4f966c64aaacda92843ee4e77c754
bijaydangol <bijaydangol11@gmail.com>
2021-07-13 21:16:29 +0545

    changes in ticket model and added update for ticket

--- model/ticket_model.js
+++ model/ticket_model.js
@@ -2,13 +2,16 @@ const mongoose=require('mongoose');
 
 const Ticket= mongoose.model('Ticket',
 {
-    location:{type:String, require:true},
+    departure:{type:String, require:true},
+    arrival:{type:String, require:true},
     driver:{type:String, require:true},
     price:{type:String},
     seat:{type:String},
     date:{type:Date},
-    time:{type:Number},
-    phone:{type:String}
+    departuretime:{type:Number},
+    arrivaltime:{type:Number},
+    phone:{type:String},
+    busno:{type:String}
 })
 
 module.exports=Ticket; 
--- route/ticket_route.js
+++ route/ticket_route.js
@@ -7,14 +7,16 @@ const { check, validationResult } = require('express-validator');
 router.post('/add/ticket',  function (req, res) {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-        const location = req.body.location;
+        const departure = req.body.departure;
+        const arrival = req.body.arrival;
         const driver = req.body.driver;
         const price = req.body.price;
         const seat = req.body.seat;
         const date = req.body.date;
-        const time = req.body.time;
+        const departuretime = req.body.departuretime;
+        const arrivaltime=req.body.arrivaltime;
         const phone = req.body.phone;
-            const store = new Ticket({ location: location, driver: driver, price: price, seat: seat, date: date, time: time, phone: phone});
+            const store = new Ticket({ departure: departure,arrival:arrival, driver: driver, price: price, seat: seat, date: date, departuretime: departuretime,arrivaltime:arrivaltime, phone: phone});
             console.log(driver);
             store.save().then(function (result) {
                 res.status(200).json({ success: true, message: "Ticket Scheduled Successfully" }) 
@@ -26,5 +28,34 @@ router.post('/add/ticket',  function (req, res) {
         res.status(201).json(errors.array());
     }
 })
+
+
+
+//updating the ticket details
+router.put('/update/ticket', function(req, res) {
+   
+        const departure = req.body.departure;
+        const arrival = req.body.arrival;
+        const driver = req.body.driver;
+        const price = req.body.price;
+        const seat = req.body.seat;
+        const date = req.body.date;
+        const departuretime = req.body.departuretime;
+        const arrivaltime=req.body.arrivaltime;
+        const phone = req.body.phone;
+        const id = req.body.id;
+    Ticket.updateOne({ _id: id }, { departure: departure,arrival:arrival, driver: driver, price: price, seat: seat, date: date, departuretime: departuretime,arrivaltime:arrivaltime, phone: phone })
+        .then(function(re) {
+            console.log(re)
+            res.status(200).json({ message: "updated" })
+        })
+        .catch(function(e) {
+            res.status(500).json({ error: e })
+        })
+   
+})
+
+
+
 
 module.exports = router;
