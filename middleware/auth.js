473612cd6302a9bf6f6a04fc432a5bcd86229f35
sam33na <saminashrestha98@gmail.com>
2021-07-16 15:59:41 +0545

    successfull authentication with valid user

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,15 @@ module.exports.verifyUser=function(req,res,next)
     try{
         const token=req.headers.authorization.split(" ")[1]
         const data=jwt.verify(token, 'anysecretkey');
-       
+        Detail.findOne({_id:data.userId}).then(function(userData)
+        {
+            req.validUser=userData;
+            console.log(userData) 
+            next();
+        })
+        .catch(function(err){
+            res.status(401).json({error:err})
+        })
     }
     catch(err)
     {
