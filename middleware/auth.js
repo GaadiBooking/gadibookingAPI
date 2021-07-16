e8d29ccd6b475749ec782bc62943c94ee7a124b0
sam33na <saminashrestha98@gmail.com>
2021-07-16 15:44:06 +0545

    try catch auth defined

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,11 +8,13 @@ module.exports.verifyUser=function(req,res,next)
    //main guard checks whether user has token or not
     console.log(req.headers.authorization)
     try{
+        const token=req.headers.authorization.split(" ")[1]
+        const data=jwt.verify(token, 'anysecretkey');
        
     }
     catch(err)
     {
-       
+        res.status(401).json({error:err})
     }
 }
 
