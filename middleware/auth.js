8f0984758aae4c2868834553d5f4bdc18e9df350
sam33na <saminashrestha98@gmail.com>
2021-07-16 15:40:52 +0545

    try catch to check user added

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,4 +3,17 @@ const Detail=require('../model/register_model');
 const router = require('../route/login_route');
 
 
+module.exports.verifyUser=function(req,res,next)
+{
+   //main guard checks whether user has token or not
+    console.log(req.headers.authorization)
+    try{
+       
+    }
+    catch(err)
+    {
+       
+    }
+}
+
 
