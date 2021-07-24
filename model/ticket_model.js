568b7befdda6f117ff675be0a5055500c6bc4be9
Sudip Rai <raisudip05@gmail.com>
2021-07-24 20:36:46 +0545

    Code modified by sudip

--- model/ticket_model.js
+++ model/ticket_model.js
@@ -4,14 +4,15 @@ const Ticket= mongoose.model('Ticket',
 {
     departure:{type:String, require:true},
     arrival:{type:String, require:true},
-    driver:{type:String, require:true},
+    driver:{type:String},
     price:{type:String},
     seat:{type:String},
-    date:{type:Date},
+    date:{type:String},
     departuretime:{type:Number},
     arrivaltime:{type:Number},
     phone:{type:String},
-    busno:{type:String}
+    busno:{type:String},
+    driver_id:{type:String},
 })
 
 module.exports=Ticket; 
--- route/ticket_route.js
+++ route/ticket_route.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const Ticket = require('../model/ticket_model');
 const auth=require('../middleware/auth')
 const { check, validationResult } = require('express-validator');
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
 
 //add ticket
 router.post('/add/ticket',auth.userVerify,auth.verifyAdmin,  function (req, res) {
     const errors = validationResult(req);
+    console.log("requested")
     if (errors.isEmpty()) {
         const departure = req.body.departure;
         const arrival = req.body.arrival;
@@ -18,7 +21,8 @@ router.post('/add/ticket',auth.userVerify,auth.verifyAdmin,  function (req, res)
         const arrivaltime=req.body.arrivaltime;
         const phone = req.body.phone;
         const busno=req.body.busno
-            const store = new Ticket({ departure: departure,arrival:arrival, driver: driver, price: price, seat: seat, date: date, departuretime: departuretime,arrivaltime:arrivaltime, phone: phone,busno:busno});
+        const driver_id=req.body.driver_id
+            const store = new Ticket({ departure: departure,arrival:arrival, driver: driver, price: price, seat: seat, date: date,driver_id:driver_id, departuretime: departuretime,arrivaltime:arrivaltime, phone: phone,busno:busno});
             store.save().then(function (result) {
                 console.log(price)
                 res.status(200).json({ success: true, message: "Ticket Scheduled Successfully" }) 
@@ -34,7 +38,7 @@ router.post('/add/ticket',auth.userVerify,auth.verifyAdmin,  function (req, res)
 
 
 //updating the ticket details
-router.put('/update/ticket',auth.userVerify,auth.verifyAdmin, function(req, res) {
+router.put('/update/ticket/:id',auth.userVerify,auth.verifyAdmin, function(req, res) {
    
         const departure = req.body.departure;
         const arrival = req.body.arrival;
@@ -46,7 +50,7 @@ router.put('/update/ticket',auth.userVerify,auth.verifyAdmin, function(req, res)
         const arrivaltime=req.body.arrivaltime;
         const phone = req.body.phone;
         const busno=req.body.busno
-        const id = req.body.id;
+        const id = req.params.id;
     Ticket.updateOne({ _id: id }, { departure: departure,arrival:arrival, driver: driver, price: price, seat: seat, date: date, departuretime: departuretime,arrivaltime:arrivaltime, phone: phone,busno:busno })
         .then(function(re) {
             console.log(re)
@@ -57,6 +61,35 @@ router.put('/update/ticket',auth.userVerify,auth.verifyAdmin, function(req, res)
         })
    
 })
+//get ticket of loged in driver
+router.get("/myticket/:id",auth.userVerify, asyncHandler(async(req,res,next)=>{
+    const id=req.params.id
+    const ticket =  await Ticket.find({driver_id:id});
+    
+    if (!ticket) {
+      return next(new ErrorResponse("Ticket not found"), 404);
+    }
+  
+    res.status(200).json({
+      message: "success",
+      data: ticket,
+    });
+  }))
+
+
+  router.get("/getTicket/:id",auth.userVerify, asyncHandler(async(req,res,next)=>{
+    const ticket = await Ticket.findById(req.params.id);
+    console.log(ticket)
+    
+    if (!ticket) {
+      return next(new ErrorResponse("Ticket not found"), 404);
+    }
+  
+    res.status(200).json({
+      message: "success",
+      data: ticket,
+    });
+  }))
 
 //delete ticket
 router.delete('/delete/ticket/:id',auth.userVerify,auth.verifyAdmin, function(req, res){
