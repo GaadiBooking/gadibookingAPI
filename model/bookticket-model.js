63acad3ecf613cd7137c30b085c1d8cabf886eb6
bijaydangol <bijaydangol11@gmail.com>
2021-07-29 20:31:25 +0545

    book ticket model

---
+++ model/bookticket-model.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const {Ticket}=require('./ticket_model')
+const {User}=require('./register_model')
+const Book= mongoose.model('BookTicket',
+{
+    userid:{
+        type:String
+    },
+    ticket:{
+        type:{Ticket},
+    }
+})
+
+module.exports=Book; 
