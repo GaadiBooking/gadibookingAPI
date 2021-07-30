ad3c7a676213a6067321f6d003310690753b059c
bijaydangol <bijaydangol11@gmail.com>
2021-07-30 18:48:17 +0545

    changes in book model and route for book ticket

--- model/bookticket-model.js
+++ model/bookticket-model.js
@@ -3,6 +3,11 @@ const {Ticket}=require('./ticket_model')
 const {User}=require('./register_model')
 const Book= mongoose.model('BookTicket',
 {
+    departure:{type:String, require:true},
+    arrival:{type:String, require:true},
+    price:{type:String},
+    seat:{type:String},
+    date:{type:String},
     userid:{
         type:String
     },
--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const bodyParser =require('body-parser');
 const db= require('./database/db');
 const login_route=require('./route/login_route');
 const ticket_route=require('./route/ticket_route');
+const book_route=require('./route/bookticket_route')
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(login_route);
 app.use(ticket_route);
+app.use(book_route)
 
 app.listen(3000);
