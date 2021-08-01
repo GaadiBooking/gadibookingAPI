97038646da6a06cf7c4754d4185201a803ddc3dc
bijaydangol <bijaydangol11@gmail.com>
2021-08-01 19:38:54 +0545

    ticketbook testing

---
+++ test/bookTicketRoute.test.js
@@ -0,0 +1,35 @@
+const book=require('../model/bookticket-model')
+const mongoose = require('mongoose');
+const url = 'mongodb://127.0.0.1:27017/GadiBooking';
+beforeAll(async() => {
+    await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    });
+});
+afterAll(async() => {
+    await mongoose.connection.close();
+});
+
+
+describe('for the book ticket route', () => {
+    // the code below is for insert testing
+    it('Inserting the data for book ticket', () => {
+        const Book = {
+            'departure': 'bhaktapur',
+            'arrival': 'kathmandu',
+            'price':'20',
+            'seat':'2a'
+        };
+
+        return book.create(Book)
+            .then((pro_ret) => {
+                expect(pro_ret.departure).toEqual('bhaktapur');
+            });
+    });
+
+
+
+
+
+});
